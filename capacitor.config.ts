a90e207f63f088a194500a3d5613ffc9c0dcfd0d
el_jijuna <pilmee@icloud.com>
2026-04-09 18:23:17 -0500

    feat: add capacitor dependencies.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.pilmee.npmlens',
+  appName: 'npm-lens',
+  webDir: 'dist'
+};
+
+export default config;
